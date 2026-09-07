2a562c8f92420874b53a08c622d4d0abaa8b0032
Vaishnavi-2728 <vaishnaviyalavarthy7@gmail.com>
2026-09-07 23:23:51 +0530

    Update app.py

--- Food_Delivery_Time_Prediction/app.py
+++ Food_Delivery_Time_Prediction/app.py
@@ -7,7 +7,9 @@ import joblib
 # Load the trained ML model
 # --------------------------------------------------
 
-model = joblib.load("best_food_delivery_model.pkl")
+model = joblib.load(
+    "Food_Delivery_Time_Prediction/best_food_delivery_model.pkl"
+)
 
 
 # --------------------------------------------------
